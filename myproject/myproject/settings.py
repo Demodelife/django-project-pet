6e04aa4470b2e3e78136401a5497aff02695c48d
Zaripov Ildar <zaripow.ildar@yandex.ru>
2023-08-07 12:47:48 +0500

    m-19 fixed debug setting 3

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -183,7 +183,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'upload/'
 
